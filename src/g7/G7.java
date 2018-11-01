6084e91ffd4276cb85749a6a4d0e03d7151367f7
Tarc <Tarc@B91>
2018-11-01 10:46:19 +0800

    updated completed ### testing cf

--- src/g7/G7.java
+++ src/g7/G7.java
@@ -17,6 +17,7 @@ public class G7 {
     public static void main(String[] args) {
         System.out.println("Hello world 1");
         System.out.println("Hello world 1");
+           System.out.println("Hello world testing");
     }
     
 }
